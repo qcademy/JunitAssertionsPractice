af2a15962fd734b73746bcb18a3160f497e3863f
Mikalai Salauyou <msalauyou@exadel.com>
2023-11-29 16:50:55 +0100

    Add readme and more methods to practice assertions

--- src/main/java/de/qcademy/junitassertionspractice/MathOperations.java
+++ src/main/java/de/qcademy/junitassertionspractice/MathOperations.java
@@ -1,15 +1,31 @@
-// MathOperations.java
-package de.qcademy.junitpractice;
+package de.qcademy.junitassertionspractice;
 
 public class MathOperations {
+
     public int add(int a, int b) {
         return a + b;
     }
 
-    public double divide(int dividend, int divisor) {
-        if (divisor == 0) {
+    public double divide(int a, int b) {
+        if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed!");
         }
-        return (double) dividend / divisor;
+        return (double) a / b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
--- src/test/java/qcademy/de/junitassertionspractice/StringUtilsTest.java
+++ src/test/java/qcademy/de/junitassertionspractice/StringUtilsTest.java
@@ -1,22 +1,35 @@
-// StringUtilsTest.java
-package de.qcademy.junitpractice;
+package qcademy.de.junitassertionspractice;
 
+import de.qcademy.junitassertionspractice.StringUtils;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringUtilsTest {
 
     @Test
     void testReverse() {
+        // Test example
+        StringUtils stringUtils = new StringUtils();
         String original = "JUnit";
-        String reversed = StringUtils.reverse(original);
+        String reversed = stringUtils.reverse(original);
         assertEquals("tinUJ", reversed, "Reversed word should match the original.");
     }
 
     @Test
     void testCapitalize() {
-        String input = "java";
-        String capitalized = StringUtils.capitalize(input);
-        assertEquals("Java", capitalized, "First character should be capitalized.");
+        // Add your test
     }
+
+    @Test
+    void testConcatenate() {
+        // Add your test
+    }
+
+    @Test
+    void testSplitIntoWords() {
+        // Add your test
+    }
+
+    // Add more tests below if necessary
 }
